c2f066677e735938c509636a37b900efbbab02f0
rando128 <sthonore@Stephanes-MacBook-Pro-3.local>
2024-11-22 07:48:26 +0100

    fix: heikin-Ashi Buy Restriction Enabled undefined

--- public/js/SymbolSettingIcon.js
+++ public/js/SymbolSettingIcon.js
@@ -574,7 +574,10 @@ class SymbolSettingIcon extends React.Component {
                                         data-state-key='buy.heikinAshiRestriction.enabled'
                                         checked={
                                           symbolConfiguration.buy
-                                            .heikinAshiRestriction.enabled
+                                            .heikinAshiRestriction
+                                            ? symbolConfiguration.buy
+                                                .heikinAshiRestriction.enabled
+                                            : false
                                         }
                                         onChange={this.handleInputChange}
                                       />
